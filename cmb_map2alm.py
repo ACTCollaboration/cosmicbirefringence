fde7c555a5498a069dae0fb09c7d4f168c34fb2e
Your Name <you@example.com>
2019-09-19 11:28:30 -0700

    update some files

--- cmb_map2alm.py
+++ cmb_map2alm.py
@@ -15,12 +15,12 @@ import curvedsky
 import prjlib
 
 
-@profile
 def cmbmap2alm(i,mtype,p,f,r):  
 
     print 'map to alm', i
 
     fmap = enmap.read_map(f.imap[mtype][i])  # load flatsky K-space combined map
+    print 'map amplitude', np.average(fmap**2)
 
     # FT
     print 'compute Fourier modes'
@@ -28,8 +28,8 @@ def cmbmap2alm(i,mtype,p,f,r):
 
     # remove some Fourier modes   
     print 'define lmask'
-    ellmin = 500
-    ellmax = 3000
+    ellmin = 300
+    ellmax = 4000
     lxcut  = 90
     lycut  = 50
     shape, wcs = fmap.shape, fmap.wcs
@@ -43,10 +43,10 @@ def cmbmap2alm(i,mtype,p,f,r):
     # transform cmb map to healpix
     print('transform to healpix')
     hpmap = enmap.to_healpix(fmap)
-    #hp.fitsfunc.write_map(f.omap[mtype][i],hpmap,overwrite=True)
 
     # from map to alm
-    hmmap = r.w * hpmap  # masking
+    hpmap = r.w * hpmap  # masking
+    #hp.fitsfunc.write_map(f.omap[mtype][i],hpmap,overwrite=True)
     alm = curvedsky.utils.hp_map2alm(p.nside,p.lmax,p.lmax,hpmap)
 
     print("save to file")
@@ -56,30 +56,39 @@ def cmbmap2alm(i,mtype,p,f,r):
 @profile
 def cmbalm2cl(p,f,r):
     #//// compute aps ////#
-    # output is ell, TT, EE, BB, TE
+    # output is ell, TT, EE, BB, TE, TB, EB
 
-    cbs = np.zeros((p.snmax,4,p.bn))
-    cls = np.zeros((p.snmax,4,p.lmax+1))
+    cbs = np.zeros((p.snmax,6,p.bn))
+    cls = np.zeros((p.snmax,6,p.lmax+1))
 
     for i in range(p.snmin,p.snmax):
 
         print('load alm', i)
 
         #load cmb alms
-        Talm = pickle.load(open(f.alm['T'][i],"rb"))
         Ealm = pickle.load(open(f.alm['E'][i],"rb"))
         Balm = pickle.load(open(f.alm['B'][i],"rb"))
+        if 'T' in p.mlist:
+            Talm = pickle.load(open(f.alm['T'][i],"rb"))
+        else:
+            Talm = 0*Ealm
 
         #compute cls
         cls[i,0,:] = curvedsky.utils.alm2cl(p.lmax,Talm)
         cls[i,1,:] = curvedsky.utils.alm2cl(p.lmax,Ealm)
         cls[i,2,:] = curvedsky.utils.alm2cl(p.lmax,Balm)
         cls[i,3,:] = curvedsky.utils.alm2cl(p.lmax,Talm,Ealm)
+        cls[i,4,:] = curvedsky.utils.alm2cl(p.lmax,Talm,Balm)
+        cls[i,5,:] = curvedsky.utils.alm2cl(p.lmax,Ealm,Balm)
         cls[i,:,:] *= 1./r.w2
 
         #compute binned cls
-        for j in range(4):
+        for j in range(6):
             cbs[i,j,:] = basic.aps.cl2bcl(p.bn,p.lmax,cls[i,j,:],spc=p.binspc)
+
+        #save cl at each rlz
+        if p.stype=='lcmb':
+            np.savetxt(f.cbi[i],np.concatenate((r.bc[None,:],cbs[i,:,:])).T)
 
     # save to files
     if p.snmax>=2:
@@ -89,15 +98,9 @@ def cmbalm2cl(p,f,r):
         np.savetxt(f.scb,np.concatenate((r.bc[None,:],np.mean(cbs[i0:,:,:],axis=0),np.std(cbs[i0:,:,:],axis=0))).T)
 
     if p.snmin==0:
-        if p.doreal:
-            print('save real')
-            np.savetxt(f.rcl,np.concatenate((r.eL[None,:],cls[0,:,:])).T)
-            np.savetxt(f.rcb,np.concatenate((r.bc[None,:],cbs[0,:,:])).T)
-        else:
-            print('save mock obs')
-            np.savetxt(f.ocl,np.concatenate((r.eL[None,:],cls[0,:,:])).T)
-            np.savetxt(f.ocb,np.concatenate((r.bc[None,:],cbs[0,:,:])).T)
-
+        print('save real')
+        np.savetxt(f.ocl,np.concatenate((r.eL[None,:],cls[0,:,:])).T)
+        np.savetxt(f.ocb,np.concatenate((r.bc[None,:],cbs[0,:,:])).T)
 
 
 if __name__ == '__main__':
@@ -106,7 +109,7 @@ if __name__ == '__main__':
     p, f, r = prjlib.init()
 
     #loop for T/E/B at each realization
-    for mtype in ['T','E','B']:
+    for mtype in p.mlist:
 
         for i in range(p.snmin,p.snmax):
 
--- prjlib.py
+++ prjlib.py
@@ -1,10 +1,10 @@
 import numpy as np
 import healpy as hp
 import sys
-import configparser
-
-#from cmblensplus
 import basic
+import curvedsky
+import configparser
+from pixell import enmap
 
 #* Define parameters
 
@@ -12,7 +12,7 @@ Tcmb = 2.72e6
 
 class params:
 
-    def __init__(self,PSA=''):
+    def __init__(self,PSA='',stype=''):
 
         #//// load config file ////#
         config = configparser.ConfigParser()
@@ -29,20 +29,24 @@ class params:
         self.olmin  = conf.getint('olmin',2)
         self.olmax  = conf.getint('olmax',3000)
         self.bn     = conf.getint('bn',30) 
-        self.binspc = conf.get('binspc','p2')
+        self.binspc = conf.get('binspc','')
 
         self.snmin  = conf.getint('snmin',0)
         self.snmax  = conf.getint('snmax',101)
-        self.stype  = conf.get('stype','lcmb')
+        if stype != '':
+            self.stype = stype
+        else:
+            self.stype = conf.get('stype','lcmb')
         if PSA!='':
             self.PSA  = PSA
         else:
             self.PSA  = conf.get('PSA','s14&15_deep56')
         self.ascale = conf.getint('ascale',1)
-        #self.doreal = True
         self.doreal = conf.getboolean('doreal',False)
+        self.chreal = conf.get('chreal','')
 
         # reconstruction
+        self.qtype  = conf.get('qtype','lens')
         self.nsidet = conf.getint('nsidet',2048)
         self.rlmin  = conf.getint('rlmin',500)
         self.rlmax  = conf.getint('rlmax',3000)
@@ -56,16 +60,29 @@ class params:
         self.psa  = self.PSA.replace('&','+')
         self.oL   = [self.olmin,self.olmax]
 
+        #mtype
+        if self.stype=='a1p0' or self.stype=='a0p3':
+            self.mlist = ['E','B']
+        else:
+            self.mlist = ['T','E','B']
+
+        #definition of T+P
+        self.qDO = [True,True,True,False,False,False]
+        self.qMV = ['TT','TE','EE']
+
         #definition of qest
-        self.qlist = ['EB']
+        self.qlist = ['TT','TE','EE','TB','EB','MV']
+        #self.qlist = ['MV']
+        if self.qtype=='rot':
+            self.qlist = ['EB']
 
 
 # * Define quad estimator names
 class quadest:
 
-    def __init__(self,qest,root,stag,ltag,otag,ids):
+    def __init__(self,qtype,qest,root,stag,ltag,otag,ids):
 
-        qtype = 'rot'
+        # qtype is the type of mode coupling, such as lens, rot, etc
         qalm = root + qtype + '/alm/'
         qrdn = root + qtype + '/rdn0/'
         qmlm = root + qtype + '/mean/'
@@ -86,10 +103,8 @@ class quadest:
         # reconstructed spectra
         self.mcls = qaps+'cl_'+qest+'_'+stag+ltag+'.dat'
         self.mcbs = qaps+'cl_'+qest+'_'+stag+ltag+otag+'.dat'
-        self.ocls = qaps+'cl_obs_'+qest+'_'+stag+ltag+'.dat'
-        self.ocbs = qaps+'cl_obs_'+qest+'_'+stag+ltag+otag+'.dat'
-        self.rcls = qaps+'cl_real_'+qest+'_'+stag+ltag+'.dat'
-        self.rcbs = qaps+'cl_real_'+qest+'_'+stag+ltag+otag+'.dat'
+        self.ocls = qaps+'cl_'+ids[0]+'_'+qest+'_'+stag+ltag+'.dat'
+        self.ocbs = qaps+'cl_'+ids[0]+'_'+qest+'_'+stag+ltag+otag+'.dat'
 
         # reconstructed alm/map and RDN0
         self.alm  = [qalm+'alm_'+qest+'_'+stag+ltag+'_'+x+'.fits' for x in ids]
@@ -100,35 +115,32 @@ class quadest:
 # * Define class filename
 class filename:
 
-    #
-    # This code assumes the following directory structure under the directory specified by "Dir" below:
+    # The code assumes the following directory structure:
     # 
-    # - curvedsky/ : outputs in curvedsky 
-    #     - cmb/ : CMB map/alm/cl in harmonic space
+    # - curvedsky/
+    #     - cmb/
     #         - alm/
     #         - aps/
-    #     - mask/  : mask defined in curvedsky
     #     - input/
-    #         - lensed.dat : input theory cl
-    #         - aalm : alms of input alpha
-    #     - rot/  : results of cosmic birefringence reconstruction
-    #         - alm/   : alm of reconstructed cosmic birefringence
-    #         - aps/   : angular power spectrum of cosmic birefringence 
-    #         - mean/  : mean-field bias
-    #         - rdn0/  : realization-dependent bias
+    #         - aalm/
+    #     - mask/ : mask defined in curvedsky
     # 
+    # - actsim/
+    #     - mask/   : mask defined in flatsky grid
     #
+
     def __init__(self,params):
 
         #//// root directories ////#
-        Dir    = '/project/projectdirs/act/data/curvedsky/'
-        # input
+        Dir    = '/global/cscratch1/sd/toshiyan/ACT/curvedsky/'
+        # input cl
         d_inp  = Dir+'input/'
-        # cmb
+        # cmb, kappa
+        d_act  = Dir+'../actsim/'
+        d_maps = Dir+'cmb/map2d_lcmb/'
         d_alm  = Dir+'cmb/alm/'
-        d_map  = Dir+'cmb/map2d/'
         d_aps  = Dir+'cmb/aps/'
-        # mask
+        # params mask
         d_msk  = Dir+'mask/'
 
         #//// basic tags ////#
@@ -137,61 +149,67 @@ class filename:
         stag = params.stype+'_'+params.psa+'_ns'+str(params.nside)+'_a'+str(params.ascale)+'deg'
 
         # output multipole range
-        otag = '_oL'+str(params.olmin)+'-'+str(params.olmax)+'_b'+str(params.bn)
+        otag = '_oL'+str(params.olmin)+'-'+str(params.olmax)+'_b'+str(params.bn)+params.binspc
 
-        # reconstruction multipole
+        # kappa reconstruction multipole
         ltag = '_l'+str(params.rlmin)+'-'+str(params.rlmax)
 
         #//// index ////#
-        ids = [str(i).zfill(5) for i in range(500)]
+        ids  = [str(i).zfill(5) for i in range(500)]
+        ids0 = [str(i).zfill(5) for i in range(500)]
         # change 1st index
-        if params.doreal: ids[0] = 'real'
+        if params.doreal:      ids[0]  = 'real'
+        if params.chreal!='':  ids[0] += '_'+params.chreal
+        #if params.absrot!='':  ids = [str(i).zfill(5)+'_absrot' for i in range(500)]
+        #if params.relrot!='':  ids = [str(i).zfill(5)+'_relrot' for i in range(500)]
 
         #//// CAMB cls ////#
         # aps of best fit cosmology
         self.lcl = d_inp+'lensed.dat'
 
         # window function
-        # mask in flat sky grid
-        self.fmask = d_msk+'/mask2d_'+params.PSA+'_'+params.stype+'.fits'
-        # mask in curvedsky healpix sphere
-        self.rmask = d_msk+'/'+params.psa+'_'+params.stype+'.fits'
-        # apodized mask in healpix
-        self.amask = d_msk+'/'+params.psa+'_'+params.stype+'_a'+str(params.ascale)+'.fits'
+        #self.fmask = d_msk+'/mask2d_'+params.PSA+'.fits'
+        self.fmask = d_act+'/prepmaps/mask_'+params.PSA+'.fits'
+        self.rmask = d_msk+'/'+params.psa+'_n'+str(params.nside)+'.fits'
+        self.amask = d_msk+'/'+params.psa+'_n'+str(params.nside)+'_a'+str(params.ascale)+'.fits'
 
-        #//// CMB, noise, input alpha, ... ////#
-        # input alpha map and alm
-        #self.amap = [d_act+'/alpha/fullskyalpha_set0_id'+str(xi)+'.fits' for xi in range(501)]
-        self.aalm = [d_inp+'/aalm/aalm_'+str(x)+'.fits' for x in ids]
+        #//// CMB, noise, kappa, alpha, ... ////#
+        self.palm = [d_act+'/alex/fullskyPhi_alm_'+x+'.fits' for x in ids0]
+        self.amap = [d_act+'/alpha/fullskyalpha_set0_id'+str(xi)+'.fits' for xi in range(501)]
+        self.aalm = [d_inp+'/aalm/aalm_'+str(x)+'.fits' for x in ids0]
 
         self.imap = {}
-        self.omap = {}
+        #self.omap = {}
         self.alm  = {}
         for mtype in ['T','E','B']:
-            # K-space combined cmb/noise maps
-            if   params.stype == 'lcmb': 
-                self.imap[mtype] = [d_map+'/K_space_prepared/preparedSimset00_Map'+x+'_'+mtype+'_'+params.PSA+'.fits' for x in ids]
-            elif params.stype == 'arot':
-                self.imap[mtype] = [d_map+'/preparedSimset00_Map'+x+'_'+mtype+'_'+params.PSA+'.fits' for x in ids]
+            if   params.stype == 'lcmb':
+                #self.imap[mtype] = [d_act+'prepmaps/preparedSimset00_Map'+x+'_'+mtype+'_'+params.PSA+'.fits' for x in ids]
+                self.imap[mtype] = [Dir+'cmb/map2d_lcmb/preparedSimset00_Map'+x+'_'+mtype+'_'+params.PSA+'.fits' for x in ids]
+            elif params.stype == 'a1p0':
+                self.imap[mtype] = [Dir+'cmb/map2d_a1p0/preparedSimset00_Map'+x+'_'+mtype+'_'+params.PSA+'.fits' for x in ids]
+            elif params.stype == 'a0p3':
+                self.imap[mtype] = [Dir+'cmb/map2d_a0p3/preparedSimset00_Map'+x+'_'+mtype+'_'+params.PSA+'.fits' for x in ids]
+            elif params.stype == 'f150':
+                self.imap[mtype] = [d_maps+'/preparedSimset00_Map'+x+'_'+mtype+'_'+params.PSA+'.fits' for x in ids]
             else:
                 print('no valid cmb maps')
-            # curvedsky map and alm
-            self.omap[mtype] = [d_map+'/'+mtype+'_'+params.stype+'_'+params.psa+'_'+x+'.fits' for x in ids] #map
-            self.alm[mtype]  = [d_alm+'/'+mtype+'_'+stag+'_'+x+'.fits' for x in ids] #alm
+            # alm
+            #self.omap[mtype] = [d_alm+'/map_'+mtype+'_'+stag+'_'+x+'.fits' for x in ids] #lensed cmb map
+            self.alm[mtype]  = [d_alm+'/'+mtype+'_'+stag+'_'+x+'.fits' for x in ids] #lensed cmb alm
             # replace sim to real
-            if params.doreal: self.imap[mtype][0] = d_map+'/preparedMap_'+mtype+'_'+params.PSA+'.fits'
+            if params.doreal: 
+                self.imap[mtype][0] = d_maps+'/preparedMap_'+mtype+'_'+params.PSA+'.fits'
 
         # cmb aps
+        self.cbi = [d_alm+'/aps_'+stag+otag+'_'+x+'.dat' for x in ids]
         self.scl = d_aps+'aps_sim_1d_'+stag+'.dat'
         self.scb = d_aps+'aps_sim_1d_'+stag+otag+'.dat'
-        self.ocl = d_aps+'aps_obs_1d_'+stag+'.dat'
-        self.ocb = d_aps+'aps_obs_1d_'+stag+otag+'.dat'
-        self.rcl = d_aps+'aps_real_1d_'+stag+'.dat'
-        self.rcb = d_aps+'aps_real_1d_'+stag+otag+'.dat'
+        self.ocl = d_aps+'aps_'+ids[0]+'_1d_'+stag+'.dat'
+        self.ocb = d_aps+'aps_'+ids[0]+'_1d_'+stag+otag+'.dat'
 
         self.quad = {}
         for q in params.qlist:
-            self.quad[q] = quadest(q,Dir,stag,ltag,otag,ids)
+            self.quad[q] = quadest(params.qtype,q,Dir,stag,ltag,otag,ids)
 
 
 class recfunc:
@@ -211,8 +229,8 @@ class recfunc:
 
 
 #initial setup
-def init(PSA=''):
-    p = params(PSA)
+def init(PSA='',stype=''):
+    p = params(PSA,stype)
     f = filename(p)
     r = recfunc(p,f)
     window(p,f,r)
@@ -223,12 +241,17 @@ def window(params,filename,r):
     #window
     if 'boss' in params.psa:
         wsf = hp.fitsfunc.read_map(filename.rmask)
+        #wrf = hp.fitsfunc.read_map(filename.Rmask)*6.02e-5
         if params.stype=='arot': wsf *= 6e-05
     if 'deep56' in params.psa:
         wsf = hp.fitsfunc.read_map(filename.rmask)
-        if params.stype=='lcmb': wsf *= 1.5
-        if params.stype=='arot': wsf *= 0.000141
+        #wsf *= 0.000141
+        #if params.stype=='lcmb': wsf *= 1.5
+        #if params.stype=='lcmb': wsf *= 0.000141
+        #if params.stype=='arot': wsf *= 0.000141
+        #wrf = hp.fitsfunc.read_map(filename.Rmask)*0.000141
 
+    print(filename.amask)
     r.w = hp.fitsfunc.read_map(filename.amask)
 
     totw = wsf*r.w
@@ -239,6 +262,7 @@ def window(params,filename,r):
 
 def make_qrec_filter(params,filename,r):
 
+    print('loading TT/EE/BB/TE from pre-computed spectrum:',filename.scl)
     ocl  = np.loadtxt(filename.scl,unpack=True,usecols=(1,2,3,4))
     r.oc = ocl
 
@@ -246,11 +270,9 @@ def make_qrec_filter(params,filename,r):
     for mtype in ['T','E','B']:
         r.Fl[mtype] = np.zeros((params.lmax+1,params.lmax+1))
 
-    # assume diagonal filtering
     for l in range(params.rlmin,params.rlmax+1):
         r.Fl['T'][l,0:l+1] = 1./ocl[0,l]
         r.Fl['E'][l,0:l+1] = 1./ocl[1,l]
         r.Fl['B'][l,0:l+1] = 1./ocl[2,l]
-
 
 
