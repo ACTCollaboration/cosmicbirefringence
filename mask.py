2674fe3f65518a8610c0c426eaee3879878ba114
Your Name <you@example.com>
2020-04-03 11:57:52 -0700

    update

--- mask.py
+++ mask.py
@@ -19,18 +19,10 @@ def apodize(frmask,famask,ascale):  # compute apodized window
     hp.fitsfunc.write_map(famask,amask,overwrite=True) # save
 
 
-
 for psa in ['s14&15_deep56','s14&15_boss']:
     p = prjlib.params(PSA=psa)
     f = prjlib.filename(p)
     transmask(p.nside,f.fmask,f.rmask)
     apodize(f.rmask,f.amask,p.ascale)
 
-# view
-#amask = hp.fitsfunc.read_map(f.omap['T'][0])
-#amask = enmap.to_healpix(enmap.read_map(f.aalm[0]))
-#from matplotlib.pyplot import *
-#hp.mollview(amask)
-#savefig('fig_amask_a'+str(p.ascale)+'deg_'+p.psa+'.png')
-#show()
 
